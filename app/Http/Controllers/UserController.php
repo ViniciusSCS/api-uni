ab7c90b9b8e5e4867b291275dd0ede7f0ea2c57a
Vinícius Siqueira <vinicius.siqueira@unievangelica.edu.br>
2024-05-10 12:36:40 -0300

    :sparkless: Adicionando CRUD de usuário

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserRequest;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $user = User::get();
+
+        return ['status' => 200, 'message' => "Usuário encontrado com sucesso!", "usuario" => $user];
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserRequest $request)
+    {
+        $data = $request->all();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => strtolower($data['email']),
+            'password' => bcrypt($data['password']),
+        ]);
+
+        return ['status' => 200, 'message' => "Usuário criado com sucesso!", "usuario" => $user];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $user = User::find($id);
+
+        if($user == null){
+            return ['status' => 200, 'message' => "Usuário não encontrado!", "usuario" => $user];
+        }
+
+        return ['status' => 200, 'message' => "Usuário encontrado com sucesso!", "usuario" => $user];
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $data = $request->all();
+        $user = User::find($id);
+
+        if($user == null){
+            return ['status' => 200, 'message' => "Usuário não encontrado!", "usuario" => $user];
+        }
+
+        $user->update($data);
+
+        return ['status' => 200, 'message' => "Usuário atualizado com sucesso!", "usuario" => $user];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $user = User::find($id);
+
+        $user->delete($id);
+
+        return ['status' => 200, 'message' => "Usuário deletado com sucesso!", "usuario" => $user];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+
+Route::post('/cadastrar', [UserController::class, 'store']);
+Route::get('/listar', [UserController::class, 'index']);
+Route::get('/visualizar/{id}', [UserController::class, 'show']);
+Route::put('/atualizar/{id}', [UserController::class, 'update']);
+Route::delete('/deletar/{id}', [UserController::class, 'destroy']);
+
+
+
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
