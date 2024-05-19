859903df37383f9e0ef3b86aaf6d16668747fc98
Vinícius Sarmento Costa Siqueira <vinicius.eng.comp92@gmail.com>
2024-05-19 16:10:03 -0300

    :sparkless: melhorando rotas.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\{
+    AuthController,
+    UserController
+};
 use Illuminate\Support\Facades\Route;
 
 /*
